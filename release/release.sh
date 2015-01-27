20b862153cb127ae4eb720a3f4da67e99979b14e
Matt Bruzek <matthew.bruzek@canonical.com>
2015-01-27 16:30:18 -0600

    Refactoring the JSON and making release.sh work with that.  Also added tags to the git repo.

--- release/release.sh
+++ release/release.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-# Copy the charms from git and bzr that are in the .config.json file.
+
+# Download the charms from git and bzr that are in the .config.json file.
+# Create a new tag which should be in the format vN.N.N
+# Copy the github code over the bzr code.
+# Push the new bzr code to launchpad.
+
+# The first argument should be the tag and it should be in the format 
+# vN.N.N to match the kubernetes version that the charms support by default.
 
 set -ex
 
-LPBASE=$(jq -r .lpbase ./.config.json)
-
-GR=$(jq -r .gitrepos[].git ./.config.json)
-CN=$(jq -r .gitrepos[].lp ./.config.json)
+GR=$(jq -r .repos[].git ./.config.json)
+LP=$(jq -r .repos[].lp ./.config.json)
 
 readarray -t gitrepos <<<"$GR"
-readarray -t charm_names <<<"$CN"
+readarray -t bzrrepos <<<"$LP"
 
 mkdir -p git
 mkdir -p lp
 
 for i in "${!gitrepos[@]}"; do
     REPO="${gitrepos[$i]}"
-    CHARM="${charm_names[$i]}"
-    TARGET="${LPBASE}/${CHARM}/trunk"
-    echo $i $REPO $CHARM
-
+    TARGET="${bzrrepos[$i]}"
+    CHARM=`basename $(dirname $TARGET)`
+    echo $i $CHARM $REPO $TARGET
 
     REPOPATH=`pwd`"/git/${CHARM}"
     if [ -d  $REPOPATH ]
@@ -47,10 +51,11 @@ for i in "${!gitrepos[@]}"; do
 
     pushd .
     cd $REPOPATH
+    git tag $1 && git push --tags
     git-vendor sync -t $1 -d $CHARMPATH
     cd $CHARMPATH
     bzr add && bzr tag $1 && bzr commit -m "Release ${1}"
-    bzr push $LPBASE/$CHARM/trunk
+    bzr push $TARGET
     popd
 
 done
